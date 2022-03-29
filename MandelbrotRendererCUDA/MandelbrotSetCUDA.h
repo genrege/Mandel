7ea3f95d9877b30b942b378a41514e4347e1e7ad
Mazen Makari <mmakari@gmail.com>
2022-03-29 23:24:12 +0100

    Performance fix for CUDA (20% faster than C++ AMP on 2070S)

--- MandelbrotRendererCUDA/MandelbrotSetCUDA.h
+++ MandelbrotRendererCUDA/MandelbrotSetCUDA.h
@@ -5,6 +5,7 @@ class __declspec(dllexport) mbrot_cuda
 {
 public:
 	mbrot_cuda();
+	~mbrot_cuda();
 	unsigned int* alloc_cuda(int size);
 	void render_mbrot(double x0, double x1, double y0, double y1, int wx, int wy, int max_iter, unsigned int* r);
 	void render_julia(double x0, double x1, double y0, double y1, double kr, double ki, int wx, int wy, int max_iter, unsigned int* r);
