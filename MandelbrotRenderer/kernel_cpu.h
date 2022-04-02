c9be6101b912fb42b2a1c4880a7a58028deb34cf
Mazen Makari <mmakari@gmail.com>
2022-04-02 22:16:47 +0100

    remove crap again

--- MandelbrotRenderer/kernel_cpu.h
+++ MandelbrotRenderer/kernel_cpu.h
@@ -4,9 +4,9 @@ namespace MathsEx
 {
     namespace kernel_cpu
     {
-        extern "C" unsigned calculate_point(double cr, double ci, unsigned max_iter);
-        extern "C" unsigned calculate_julia_point(double cr, double ci, double kr, double ki, unsigned maxIters);
-        extern "C" void mandelbrot_kernel(unsigned display_w, unsigned display_h, double x0, double x1, double y0, double y1, unsigned max_iter, unsigned* iters);
-        extern "C" void julia_kernel(unsigned display_w, unsigned display_h, double x0, double x1, double y0, double y1, double kr, double ki, unsigned max_iter, unsigned* iters);
+        unsigned calculate_point(double cr, double ci, unsigned max_iter);
+        unsigned calculate_julia_point(double cr, double ci, double kr, double ki, unsigned maxIters);
+        void mandelbrot_kernel(unsigned display_w, unsigned display_h, double x0, double x1, double y0, double y1, unsigned max_iter, unsigned* iters);
+        void julia_kernel(unsigned display_w, unsigned display_h, double x0, double x1, double y0, double y1, double kr, double ki, unsigned max_iter, unsigned* iters);
    }
 }
